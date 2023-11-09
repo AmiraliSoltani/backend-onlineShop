d9f0d6341e014dad9b6f309e6dbc989dfc84199b
Amirali Soltani <97620073+AmiraliSoltani@users.noreply.github.com>
2023-11-09 12:30:41 -0500

    make changes for vercel6

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(
     extended: true,
   })
 );
-app.use(express.static("public"));
+//app.use(express.static("public"));
 
 // mongoose.connect("mongodb://127.0.0.1:27017/onlineShop", {
 //   useNewUrlParser: true,
