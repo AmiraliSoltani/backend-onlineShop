401c82dbcf63571881dba395c5990b5929cc3aa2
Amirali Soltani <97620073+AmiraliSoltani@users.noreply.github.com>
2023-11-09 14:12:16 -0500

    make changes for vercel16

---
+++ api/categories.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+// Import your Mongoose model
+
+mongoose.connect(
+  "mongodb+srv://asoltani7:wXxeR5GlT4n4X6z1@cluster0.efuoscy.mongodb.net/onlineShop?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+const categorySchema = new mongoose.Schema({
+  categoryPicture: String,
+  iconPic: String,
+  description: String,
+  id: Number,
+  order: Number,
+  parentId: Number,
+  title: String,
+});
+
+const Category = mongoose.model("categories", categorySchema);
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
+
+  try {
+    // Use the "Product" model to find all products
+    const foundCategories = await Category.find();
+    console.log(foundCategories);
+    res.status(200).json(foundCategories);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error while fetching categories" });
+  }
+}
