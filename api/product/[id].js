cceba70bdce9f29ef2a2074f431234f5116baab2
Amirali Soltani <97620073+AmiraliSoltani@users.noreply.github.com>
2023-11-09 15:24:02 -0500

    make changes for vercel18

---
+++ api/product/[id].js
@@ -0,0 +1,86 @@
+// api/products.js
+
+const mongoose = require("mongoose");
+
+// Import your Mongoose model
+
+mongoose.connect(
+  "mongodb+srv://asoltani7:wXxeR5GlT4n4X6z1@cluster0.efuoscy.mongodb.net/onlineShop?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
+const productSchema = new mongoose.Schema({
+  title: String,
+  _id: Number,
+  comments: [
+    {
+      vote: Number,
+      memberName: String,
+      date: {
+        day: Number,
+        month: Number,
+        year: Number,
+      },
+      srcOfAvatar: Number,
+      data: {
+        title: String,
+        body: String,
+      },
+    },
+  ],
+  title_En: String,
+  description: String,
+  price: Number,
+  off: String,
+  offerTime: String,
+  categoryId: Number,
+  categoryAttributes: [
+    {
+      id: String,
+      items: [
+        {
+          id: Number,
+          attItem: Number,
+        },
+      ],
+      count: Number,
+    },
+  ],
+  guarantee: {
+    hasGuarantee: Boolean,
+    guranteeDate: String,
+    guranteeName: String,
+  },
+  productPic: {
+    grey: String,
+    grey2: String,
+    grey3: String,
+    grey4: String,
+  },
+  videoUrl: String,
+  vote: Number,
+  dailyRentalRate: [Number],
+  visited: Number,
+  sold: Number,
+});
+const Product = mongoose.model("products", productSchema);
+
+// api/products/[id].js
+
+module.exports = async (req, res) => {
+  const productId = req.query.id;
+
+  try {
+    const foundProduct = await Product.findById(productId);
+    if (foundProduct) {
+      console.log(foundProduct);
+      res.send(foundProduct);
+    } else {
+      res.status(404).send("Product not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error while fetching product");
+  }
+};
