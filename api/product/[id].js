250cb67adf4bf78551e6014207235388107d9170
Amirali Soltani <97620073+AmiraliSoltani@users.noreply.github.com>
2023-11-15 16:40:08 -0500

    changes for vercel 42

--- api/product/[id].js
+++ api/product/[id].js
@@ -78,7 +78,7 @@ module.exports = async (req, res) => {
       console.log(foundProduct);
       res.send(foundProduct);
     } else {
-      console.log("hiiiiiiii");
+      console.log(req);
       res.status(404).send(`Product with ID not found`);
     }
   } catch (error) {
