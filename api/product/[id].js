537e9f252e2a461589b4d3ce40838f47241084f7
Amirali Soltani <97620073+AmiraliSoltani@users.noreply.github.com>
2023-11-16 00:31:00 -0500

    changes for vercel 64

--- api/product/[id].js
+++ api/product/[id].js
@@ -74,12 +74,22 @@ const productSchema = new mongoose.Schema({
 const Product = mongoose.model("products", productSchema);
 
 async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
 
   if (req.method === "GET") {
     const productId = req.query.id;
