e1f53fa26305813e5388ac73b5212d3fc3573b06
Amirali Soltani <97620073+AmiraliSoltani@users.noreply.github.com>
2023-11-16 00:15:51 -0500

    changes for vercel 64

--- api/product/[id].js
+++ api/product/[id].js
@@ -1,249 +1,150 @@
-const http = require("http");
-const url = require("url");
+// api/products.js
+const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
-const cors = require("micro-cors");
+const cors = require("micro-cors")({
+  allowMethods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+  allowHeaders: ["Authorization", "Content-Type"],
+  origin: "http://localhost:3000", // Replace with the origin of your React app
+});
+// Import your Mongoose model
 
-// Create a server
-const server = http.createServer(async (req, res) => {
-  const { method, url: reqUrl } = req;
-  const parsedUrl = url.parse(reqUrl, true);
+// Import your Mongoose model
 
-  // Enable CORS
+mongoose.connect(
+  "mongodb+srv://asoltani7:wXxeR5GlT4n4X6z1@cluster0.efuoscy.mongodb.net/onlineShop?retryWrites=true&w=majority",
+  {
+    useUnifiedTopology: true,
+  }
+);
+const productSchema = new mongoose.Schema({
+  title: String,
+  _id: Number,
+  comments: [
+    {
+      vote: Number,
+      memberName: String,
+      date: {
+        day: Number,
+        month: Number,
+        year: Number,
+      },
+      srcOfAvatar: Number,
+      data: {
+        title: String,
+        body: String,
+      },
+    },
+  ],
+  title_En: String,
+  description: String,
+  price: Number,
+  off: String,
+  offerTime: String,
+  categoryId: Number,
+  categoryAttributes: [
+    {
+      id: String,
+      items: [
+        {
+          id: Number,
+          attItem: Number,
+        },
+      ],
+      count: Number,
+    },
+  ],
+  guarantee: {
+    hasGuarantee: Boolean,
+    guranteeDate: String,
+    guranteeName: String,
+  },
+  productPic: {
+    grey: String,
+    grey2: String,
+    grey3: String,
+    grey4: String,
+  },
+  videoUrl: String,
+  vote: Number,
+  dailyRentalRate: [Number],
+  visited: Number,
+  sold: Number,
+});
+const Product = mongoose.model("products", productSchema);
+
+async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE"
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"
   );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+  res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
 
-  // Establish MongoDB connection
-  mongoose.connect(
-    "mongodb+srv://asoltani7:wXxeR5GlT4n4X6z1@cluster0.efuoscy.mongodb.net/onlineShop?retryWrites=true&w=majority",
-    {
-      useUnifiedTopology: true,
-    }
-  );
-
-  // Define your Mongoose model
-  const Product = mongoose.model("products", productSchema);
-
-  // Handle GET request
-  if (method === "GET" && parsedUrl.pathname === "/product") {
-    const productId = parsedUrl.query.id;
+  if (req.method === "GET") {
+    const productId = req.query.id;
+    console.log(req.method);
+    console.log("hiiiiiiiii");
 
     try {
       const foundProduct = await Product.findById(productId);
       if (foundProduct) {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify(foundProduct));
+        res.send(foundProduct);
       } else {
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        res.end(`Product with ID ${productId} not found`);
+        res.status(404).send(`Product with ID not found`);
       }
     } catch (error) {
       console.error(error);
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Error while fetching product");
+      res.status(500).send("Error while fetching product");
     }
-  }
+  } else if (req.method === "PATCH") {
+    res.setHeader("Content-Type", "application/json"); // Set the content type if not set
 
-  // Handle PATCH request
-  else if (method === "PATCH" && parsedUrl.pathname === "/product") {
-    // Retrieve data from the request body
-    let requestBody = "";
-    req.on("data", (chunk) => {
-      requestBody += chunk.toString();
-    });
+    console.log("heeeeeeeeeee");
+    console.log(req.body);
+    const bodyData = JSON.parse(req.body);
 
-    req.on("end", async () => {
-      const bodyData = JSON.parse(requestBody);
-      const productId = parsedUrl.query.id;
-      const { vote, memberName, data, srcOfAvatar } = bodyData;
+    const productId = req.query.id;
+    const { vote, memberName, data, srcOfAvatar } = bodyData;
+    console.log(memberName);
+    console.log("shttttttttttt");
+    console.log(srcOfAvatar);
 
-      try {
-        const foundProduct = await Product.findById(productId);
-        if (foundProduct) {
-          // Modify the found product based on the PATCH request body
-          // ...
+    try {
+      const foundProduct = await Product.findById(productId);
+      if (foundProduct) {
+        // Add a new comment to the beginning of the product's comments array
+        foundProduct.comments.unshift({
+          vote: vote,
+          memberName: memberName,
+          date: {
+            day: new Date().getDate(),
+            month: new Date().getMonth() + 1,
+            year: new Date().getFullYear(),
+          },
+          srcOfAvatar: srcOfAvatar,
+          data: {
+            title: data.title,
+            body: data.body,
+          },
+        });
 
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(foundProduct));
-        } else {
-          res.writeHead(404, { "Content-Type": "text/plain" });
-          res.end(`Product with ID ${productId} not found`);
-        }
-      } catch (error) {
-        console.error(error);
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Error while updating product");
+        // Save the updated product with the new comment
+        const updatedProduct = await foundProduct.save();
+        res.status(200).send(updatedProduct);
+      } else {
+        res.status(404).send(`Product with ID ${productId} not found`);
       }
-    });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error while updating product");
+    }
+  } else {
+    res.status(405).send("Method Not Allowed");
   }
+}
 
-  // Handle other request methods or paths
-  else {
-    res.writeHead(405, { "Content-Type": "text/plain" });
-    res.end("Method Not Allowed");
-  }
-});
+const corsHandler = cors(handler);
 
-// Set up CORS middleware
-const corsHandler = cors({
-  allowMethods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-  allowHeaders: ["Authorization", "Content-Type"],
-  origin: "http://localhost:3000", // Replace with your frontend URL
-});
-
-// Apply CORS to the server
-const serverWithCors = corsHandler(server);
-
-// Start the server
-const PORT = 3000;
-serverWithCors.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-// // api/products.js
-// const express = require("express");
-// const app = express();
-// const mongoose = require("mongoose");
-// const cors = require("micro-cors")({
-//   allowMethods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
-//   allowHeaders: ["Authorization", "Content-Type"],
-//   origin: "http://localhost:3000", // Replace with the origin of your React app
-// });
-// // Import your Mongoose model
-
-// // Import your Mongoose model
-
-// mongoose.connect(
-//   "mongodb+srv://asoltani7:wXxeR5GlT4n4X6z1@cluster0.efuoscy.mongodb.net/onlineShop?retryWrites=true&w=majority",
-//   {
-//     useUnifiedTopology: true,
-//   }
-// );
-// const productSchema = new mongoose.Schema({
-//   title: String,
-//   _id: Number,
-//   comments: [
-//     {
-//       vote: Number,
-//       memberName: String,
-//       date: {
-//         day: Number,
-//         month: Number,
-//         year: Number,
-//       },
-//       srcOfAvatar: Number,
-//       data: {
-//         title: String,
-//         body: String,
-//       },
-//     },
-//   ],
-//   title_En: String,
-//   description: String,
-//   price: Number,
-//   off: String,
-//   offerTime: String,
-//   categoryId: Number,
-//   categoryAttributes: [
-//     {
-//       id: String,
-//       items: [
-//         {
-//           id: Number,
-//           attItem: Number,
-//         },
-//       ],
-//       count: Number,
-//     },
-//   ],
-//   guarantee: {
-//     hasGuarantee: Boolean,
-//     guranteeDate: String,
-//     guranteeName: String,
-//   },
-//   productPic: {
-//     grey: String,
-//     grey2: String,
-//     grey3: String,
-//     grey4: String,
-//   },
-//   videoUrl: String,
-//   vote: Number,
-//   dailyRentalRate: [Number],
-//   visited: Number,
-//   sold: Number,
-// });
-// const Product = mongoose.model("products", productSchema);
-
-// async function handler(req, res) {
-//   if (req.method === "GET") {
-//     const productId = req.query.id;
-//     console.log(req.method);
-//     console.log("hiiiiiiiii");
-
-//     try {
-//       const foundProduct = await Product.findById(productId);
-//       if (foundProduct) {
-//         res.send(foundProduct);
-//       } else {
-//         res.status(404).send(`Product with ID not found`);
-//       }
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).send("Error while fetching product");
-//     }
-//   } else if (req.method === "PATCH") {
-//     console.log("heeeeeeeeeee");
-//     console.log(req.body);
-//     const bodyData = JSON.parse(req.body);
-
-//     const productId = req.query.id;
-//     const { vote, memberName, data, srcOfAvatar } = bodyData;
-//     console.log(memberName);
-//     console.log("shttttttttttt");
-//     console.log(srcOfAvatar);
-
-//     try {
-//       const foundProduct = await Product.findById(productId);
-//       if (foundProduct) {
-//         // Add a new comment to the beginning of the product's comments array
-//         foundProduct.comments.unshift({
-//           vote: vote,
-//           memberName: memberName,
-//           date: {
-//             day: new Date().getDate(),
-//             month: new Date().getMonth() + 1,
-//             year: new Date().getFullYear(),
-//           },
-//           srcOfAvatar: srcOfAvatar,
-//           data: {
-//             title: data.title,
-//             body: data.body,
-//           },
-//         });
-
-//         // Save the updated product with the new comment
-//         const updatedProduct = await foundProduct.save();
-//         res.status(200).send(updatedProduct);
-//       } else {
-//         res.status(404).send(`Product with ID ${productId} not found`);
-//       }
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).send("Error while updating product");
-//     }
-//   } else {
-//     res.status(405).send("Method Not Allowed");
-//   }
-// }
-
-// const corsHandler = cors(handler);
-
-// // Apply CORS to the handler and export it
-// module.exports = corsHandler;
+// Apply CORS to the handler and export it
+module.exports = corsHandler;
